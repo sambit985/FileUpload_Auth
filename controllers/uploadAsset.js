9dee8e5197190490776c9afed76f642fd3083512
sambit985 <kumarsambit678@gmail.com>
2023-12-28 23:20:10 +0530

    Updated file :boom:

--- controllers/uploadAsset.js
+++ controllers/uploadAsset.js
@@ -1,10 +1,11 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../uploads");
+    // Use an absolute path for the destination directory
+    const absolutePath = path.resolve(__dirname, "..", "uploads");
+    cb(null, absolutePath);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -16,10 +17,14 @@ const upload = multer({ storage: storage });
 const uploadAsset = (req, res) => {
   if (!req.file) {
     console.log("No file uploaded");
-    return res.status(400).json({ message: "No file has uploded" });
+    return res.status(400).json({ message: "No file has uploaded" });
   }
+
   console.log(req.file);
-  const filePath = path.join(__dirname, "..", "uploads", req.file.filename);
+
+  const projectDir = path.join(__dirname, "..");
+  const filePath = path.join(projectDir, "uploads", req.file.filename);
+
   return res
     .status(200)
     .json({ message: "File uploaded successfully", filePath });
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 //import controller
-const { uploadController, upload } = require("../controllers/uploadAsset");
+const { uploadAsset, upload } = require("../controllers/uploadAsset");
 
 //create route for file uplaod
-router.post("/upload", upload.single("file"), uploadController);
+router.post("/upload", upload.single("file"), uploadAsset);
 
 module.exports = router;
